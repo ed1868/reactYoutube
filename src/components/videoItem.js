49855da4fdf5e8f1ad4f9574e439a05b8f326058
Eddie Ruiz <ruizeduardo21@gmail.com>
2019-05-21 21:59:40 -0400

    video item compontent added

---
+++ src/components/videoItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class videoItem extends Component {
+  render() {
+    return (
+      <div>
+        Video Item
+      </div>
+    )
+  }
+}
--- src/components/videoList.js
+++ src/components/videoList.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import VideoItem from './videoItem';
 
 export default class videoList extends Component {
   render() {
+    const renderedList = this.props.videos.map((video) => {
+      return <VideoItem />
+    })
     return (
       <div>
-        videoList 
+        {renderedList}
       </div>
     )
   }
